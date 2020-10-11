45815a91cfe98975018cc6b26c4fa7b4c9b5b7cc
clearloop <udtrokia@gmail.com>
2020-10-11 11:20:33 +0800

    feat(frame): add lastConfirmed API in ethereumRelay

--- src/frame/ethereum/relay.rs
+++ src/frame/ethereum/relay.rs
@@ -1,8 +1,17 @@
 //! Ethereum Relay
-
+use codec::Encode;
+use core::marker::PhantomData;
 use substrate_subxt::system::{System, SystemEventsDecoder};
-use substrate_subxt_proc_macro::module;
+use substrate_subxt_proc_macro::{module, Store};
 
 /// Ethereum Relay Pallet
 #[module]
 pub trait EthereumRelay: System {}
+
+/// PendingHeaders Storage
+#[derive(Clone, Debug, Eq, PartialEq, Store, Encode)]
+pub struct ConfirmedBlockNumbers<T: EthereumRelay> {
+    #[store(returns = Vec<u64>)]
+    /// Runtime marker
+    pub _runtime: PhantomData<T>,
+}
