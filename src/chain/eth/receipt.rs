2ef5063e865573a36e7b5abaa943699f27311ed5
clearloop <udtrokia@gmail.com>
2020-10-11 18:32:57 +0800

    feat(eth): add receipt json type

--- src/chain/eth/receipt.rs
+++ src/chain/eth/receipt.rs
@@ -1,4 +1,5 @@
 //! Ethereum receipt
+use crate::{bytes, hex};
 use codec::Encode;
 
 /// Redeem for
@@ -25,4 +26,41 @@ pub struct EthereumReceiptProof {
     pub proof: Vec<u8>,
     /// Ethereum Header Hash
     pub header_hash: [u8; 32],
+}
+
+/// Ethereum Receipt Proof Json
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
+pub struct EthereumReceiptProofJson {
+    /// Proof index
+    pub index: String,
+    /// Receipt Proof
+    pub proof: String,
+    /// Ethereum Header Hash
+    pub header_hash: String,
+}
+
+impl Into<EthereumReceiptProofJson> for EthereumReceiptProof {
+    fn into(self) -> EthereumReceiptProofJson {
+        EthereumReceiptProofJson {
+            index: format!("{:x}", self.index),
+            proof: hex!(self.proof),
+            header_hash: hex!(self.header_hash.to_vec()),
+        }
+    }
+}
+
+impl Into<EthereumReceiptProof> for EthereumReceiptProofJson {
+    fn into(self) -> EthereumReceiptProof {
+        let index = if self.index.starts_with("0x") {
+            "00"
+        } else {
+            &self.index[2..]
+        };
+
+        EthereumReceiptProof {
+            index: u64::from_str_radix(index, 16).unwrap_or(0),
+            proof: bytes!(self.proof.as_str()),
+            header_hash: bytes!(self.header_hash.as_str(), 32),
+        }
+    }
 }
