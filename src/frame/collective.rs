1aa2cdea691c9351797362ae6a1c3e6ef3707d4f
clearloop <udtrokia@gmail.com>
2020-10-20 17:46:45 +0800

    feat(frame): complete collective frame

--- src/frame/collective.rs
+++ src/frame/collective.rs
@@ -2,8 +2,11 @@
 
 use codec::{Decode, Encode};
 use core::marker::PhantomData;
-use substrate_subxt::system::{System, SystemEventsDecoder};
-use substrate_subxt_proc_macro::{module, Store};
+use substrate_subxt::{
+    system::{System, SystemEventsDecoder},
+    Encoded,
+};
+use substrate_subxt_proc_macro::{module, Call, Store};
 
 /// The subset of the `frame_sudo::Trait` that a client must implement.
 #[module]
@@ -15,4 +18,15 @@ pub struct Members<T: Council> {
     #[store(returns = Vec<<T as System>::AccountId>)]
     /// Runtime marker.
     pub _runtime: PhantomData<T>,
+}
+
+/// Execute a transaction with sudo permissions.
+#[derive(Clone, Debug, Eq, PartialEq, Call, Encode)]
+pub struct Execute<'a, T: Council> {
+    /// Runtime marker.
+    pub _runtime: PhantomData<T>,
+    /// Encoded transaction.
+    pub proposal: &'a Encoded,
+    /// Proposal length.
+    pub bound: u32,
 }
